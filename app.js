4287741119817c55a21de9312c2c2b015421971c
penguin5555 <116239681+penguin5555@users.noreply.github.com>
2023-10-27 21:37:56 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const requestIp = require('request-ip');
 const app = express();
 const axios = require('axios');
 const request = require('request');
+const IP = require('ip');
+const API_KEY = '2ccd1bd324bd42e4ae230af59c37ad96';
+const URL = 'https://ipgeolocation.abstractapi.com/v1/?api_key=' + API_KEY;
 
 const port = 3000;
 
@@ -12,9 +15,15 @@ app.get('/', (req, res) => {
     res.redirect('/home')
 });
 
+const sendAPIRequest = async (ipAddress) => {
+    const apiResponse = await axios.get(URL + "&ip_address=" + ipAddress);
+    return apiResponse.data;
+}
+
 app.get('/home', async (req, res) => {
-    
-    
+    const ipAddress = IP.address();
+    const ipAddressInformation = await sendAPIRequest(ipAddress);
+    console.log(ipAddressInformation, ipAddress)
     try {
         const response = await axios.get('https://api.ipify.org?format=json');
         const clientIP = response.data.ip;
@@ -24,7 +33,7 @@ app.get('/home', async (req, res) => {
         console.error('Failed to fetch client IP address:', error);
         res.status(500).json({ error: 'Failed to fetch IP address' });
         }
-    });
+});
 
 app.listen(port, () => {
     console.log("Server running!")
